def783a489acc07fabbfcb3fc8d06e1bcfca5efd
João Vítor Sberse <jotabassosberse@gmail.com>
2022-02-04 17:33:32 -0300

    little progress

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import React from 'react';
 import Header1 from './components/headers/header1';
 import Header2 from './components/headers/header2';
 import HeaderMobo from './components/headers/headermobo';
+import Body from './components/body/body.js';
 import { useMediaQuery } from '@chakra-ui/react'
+import Navbuttons from './components/body/navbuttons';
 
 function App() {
 
-  const [isLargerThan780] = useMediaQuery('(min-width: 780px)')
+  const [isLargerThan768] = useMediaQuery('(min-width: 768px)')
 
 
   return (
     <>
-      {isLargerThan780 ? <Header2 /> : <Header1 />}
-      <HeaderMobo></HeaderMobo>
+      {isLargerThan768 ? <Header2 /> : <Header1 />}
+      <Navbuttons></Navbuttons>
+      <Body></Body>
     </>
   );
 }
--- src/components/headers/headermobo.js
+++ src/components/headers/headermobo.js
@@ -1,9 +1,13 @@
-import { Box, Heading, Text, Image, Icon } from "@chakra-ui/react"
+import { Box, Heading, Text, Image, Icon, useMediaQuery } from "@chakra-ui/react"
 import Pfp from '../../images/pfp.png'
 
 const Headermobo = () => {
+
+    const [isLargerThan543] = useMediaQuery('(min-width: 543px)')
+
+
     return (
-        <Box p='0 16px'>
+        <Box p='0 16px' bg={isLargerThan543 ? 'color.dark' : 'bg'}>
             <Box display='flex' h='auto' alignItems='center' m='0px 0px 24px 0px'>
                 <Box w='20%' maxW='120px' verticalAlign='center' position='relative' >
                     <Image src={Pfp} borderRadius='full' w='90%' maxW='119px' border='1px solid rgba(255, 255, 255, 0.3)' />
@@ -32,17 +36,17 @@ const Headermobo = () => {
                 </Box>
             </Box>
             <Box display='flex' alignItems='center' >
-                <Box display='flex'>
+                <Box display='flex' alignItems='self-end'>
                     <Icon className='beBlue1' viewBox="-4 -5 24 24" fill='rgba(255, 255, 255, 0.6)' boxSize={6}>
                     <path d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"></path>
                     </Icon>
-                    <Text m='0 3px 0 0' fontWeight='500' color='rgba(255, 255, 255, 0.8)'>0</Text>
-                    <Text fontWeight='300' color='rgba(255, 255, 255, 0.4)'>followers</Text>
+                    <Text m='0 3px 0 0' fontSize='1rem' fontWeight='500' color='rgba(255, 255, 255, 0.8)'>0</Text>
+                    <Text fontWeight='300' fontSize='0.9rem' color='rgba(255, 255, 255, 0.4)'>followers</Text>
                 </Box>
                 <Text m='0 4px' fontSize='12px' color='white'>•</Text>
-                <Box display='flex'>
-                    <Text  m='0 3px 0 0' fontWeight='500' color='rgba(255, 255, 255, 0.8)'>1</Text>
-                    <Text fontWeight='300' color='rgba(255, 255, 255, 0.4)'>following</Text>
+                <Box display='flex' alignItems='self-end'>
+                    <Text  m='0 3px 0 0' fontSize='1rem' fontWeight='500'  color='rgba(255, 255, 255, 0.8)'>1</Text>
+                    <Text fontWeight='300' fontSize='0.9rem' color='rgba(255, 255, 255, 0.4)'>following</Text>
                 </Box>
             </Box>
         </Box >
